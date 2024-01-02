ffd8fe71a106bfc458526f52c64b2a093ae28961
Usieme Blessing <usiemeblessing@mail.com>
2024-01-02 17:03:44 +0100

    modified files

--- js/script.js
+++ js/script.js
@@ -61,12 +61,36 @@ const obs = new IntersectionObserver(
 		const ent = entries[0];
 		console.log(ent);
 		if (ent.isIntersecting === false) {
-			document.querySelector(".header").classList.add("sticky");
+			document.body.classList.add("sticky");
+		}
+		if (ent.isIntersecting) {
+			document.body.classList.remove("sticky");
 		}
 	},
 	{
 		root: null,
 		threshold: 0,
+		rootMargin: "-80px",
 	}
 );
 obs.observe(sectionHeroEl);
+
+//////////////////////////////////////////////
+//Fixing Safari flexbox gap
+function checkFlexGap() {
+	var flex = document.createElement("div");
+	flex.style.display = "flex";
+	flex.style.flexDirection = "column";
+	flex.style.rowGap = "1px";
+
+	flex.appendChild(document.createElement("div"));
+	flex.appendChild(document.createElement("div"));
+
+	document.body.appendChild(flex);
+	var isSupported = flex.scrollHeight === 1;
+	flex.parentNode.removeChild(flex);
+	console.log(isSupported);
+
+	if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
