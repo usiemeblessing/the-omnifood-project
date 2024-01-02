a100433fbb81ed30eb949b5ce3005f5d8081c88a
Usieme Blessing <usiemeblessing@mail.com>
2024-01-02 14:05:44 +0100

    modified files

--- js/script.js
+++ js/script.js
@@ -24,3 +24,49 @@ btnNavEl.addEventListener("click", function () {
 });
 
 //////////////////////////////////////////////
+//Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+	link.addEventListener("click", function (e) {
+		e.preventDefault();
+		const href = link.getAttribute("href");
+
+		// Scroll back to top
+		if (href == "#")
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+
+		//scroll to other links
+		if (href != "#" && href.startsWith("#")) {
+			const sectionEl = document.querySelector(href);
+			sectionEl.scrollIntoView({ behavior: "smooth" });
+		}
+
+		//close mobile navigation
+		if (link.classList.contains("main-nav-link"))
+			headerEl.classList.toggle("nav-open");
+	});
+});
+
+//////////////////////////////////////////////
+//Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+	function (entries) {
+		const ent = entries[0];
+		console.log(ent);
+		if (ent.isIntersecting === false) {
+			document.querySelector(".header").classList.add("sticky");
+		}
+	},
+	{
+		root: null,
+		threshold: 0,
+	}
+);
+obs.observe(sectionHeroEl);
